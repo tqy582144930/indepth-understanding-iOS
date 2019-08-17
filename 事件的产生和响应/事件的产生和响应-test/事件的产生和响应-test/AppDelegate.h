167aed72538652d40d9170f3361650fd1a0d55be
tqy582144930 <582144930@qq.com>
2019-08-17 22:56:28 +0800

    响应者链

---
+++ 事件的产生和响应/事件的产生和响应-test/事件的产生和响应-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  事件的产生和响应-test
+//
+//  Created by _祀梦 on 2019/8/17.
+//  Copyright © 2019 涂强尧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 事件的产生和响应/事件的产生和响应-test/事件的产生和响应-test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  事件的产生和响应-test
+//
+//  Created by _祀梦 on 2019/8/17.
+//  Copyright © 2019 涂强尧. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    
+}
+
+@end
