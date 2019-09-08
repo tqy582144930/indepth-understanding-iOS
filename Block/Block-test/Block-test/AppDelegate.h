d674687df41a0f2ba033cfd7224a2ead45320d1d
tqy582144930 <582144930@qq.com>
2019-09-08 23:14:41 +0800

    Block

---
+++ Block/Block-test/Block-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Block-test
+//
+//  Created by _祀梦 on 2019/9/8.
+//  Copyright © 2019 涂强尧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
